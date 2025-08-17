229e8b1fa58fb3ed23331ebf4af42c89cd262722
Randyy <rynnkunnnn@gmail.com>
2025-08-17 22:54:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,54 +2,48 @@ const config = {
     PORT: process.env.PORT || 3000,
     settings: {
         design: {
-            background_hex: '#DAEAFF',
+            background_hex: '#333',
             header_image: null,
             layout: ['projects', 'links', 'interests']
         },
         profile: {
             personal: {
                 profile_url: '/profile_img.jpg',
-                name: 'Rynn',
+                name: 'Randy Yuan Kurnianto',
                 is_verified: true,
-                pronunciation: 'Rinn',
+                pronunciation: null,
                 pronouns: 'He/She?',
                 location: 'Sragen, Jawa Tengah',
                 about: 'Just a newbie exploring the world of coding.'
             },
             professional: {
                 job_title: 'Web Developer',
-                company: 'Nekorinn Folders.'
+                company: 'NekoLabs.'
             },
             projects: [
                 {
-                    thumbnail: '/hydromind_img.png',
-                    name: 'HydroMind AI',
-                    desc: 'A powerful AI assistant built to help with everyday tasks and answer questions efficiently.',
-                    link: 'https://mind.hydrooo.web.id'
-                },
-                {
-                    thumbnail: '/rynnui_img.png',
+                    thumbnail: null,
                     name: 'Rynn UI (Open Source)',
                     desc: 'A simple and easy-to-use API documentation interface built with Express.js.',
                     link: 'https://github.com/rynn-k/Rynn-UI'
                 },
                 {
-                    thumbnail: '/nekorinn_img.png',
-                    name: 'Nekorinn API\'s',
+                    thumbnail: null,
+                    name: 'NekoAPIs',
                     desc: 'A streamlined REST API service designed for developers who need quick access to multiple API functionalities in one place.',
-                    link: 'https://api.nekorinn.my.id'
+                    link: 'https://api.nekoo.qzz.io'
                 },
                 {
-                    thumbnail: '/nekonime_img.png',
-                    name: 'Nekonime',
+                    thumbnail: null,
+                    name: 'NekOtaku.',
                     desc: 'An anime streaming platform with a minimalist, ad-free, and user-friendly interface for a seamless viewing experience.',
-                    link: 'https://nekonime.nekorinn.my.id'
+                    link: 'https://otaku.nekoo.qzz.io'
                 },
                 {
-                    thumbnail: '/mnmlistai_img.png',
+                    thumbnail: null,
                     name: 'Mnmlist AI',
                     desc: 'A minimalist AI tool designed for simplicity and clarity. Focus on what matters—no clutter, just intelligent solutions.',
-                    link: 'https://mnmlist.up.railway.app'
+                    link: 'https://mnmlist.qzz.io'
                 }
             ],
             links: [
